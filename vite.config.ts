1fb056f90e178bbb7eb8a82a16d93c2cb6bfc484
Grace Rarer <gracerarer@gmail.com>
2024-10-29 23:02:18 -0700

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		preact({
-			prerender: {
-				enabled: false,				
-			},
-		}),
+		preact(),
+		VitePWA({registerType: "prompt"})
 	],
 	base: '/check-splitter/',
 });
